790bfcd91f2ff19a71f1b71ae550845efea35b60
Seuleeee <jhs931224@gmail.com>
2020-04-03 15:49:41 +0900

    decorlator function 연습문제 풀어보기

--- decorlator_function.py
+++ decorlator_function.py
@@ -1,6 +1,44 @@
-a = int(input())
-b = int(input())
-print(a*(b%10))
-print(a*((b-(100*(b//100)))//10))
-print(a*(b//100))
-print(a*b)
+# ID와 Password가 일치해야만 함수가 동작하도록 만들기
+import logging
+import random
+datas = [
+    {"id":"Kobe", "pwd":"Bryant", "addr":"LA"},
+    {"id":"Michael", "pwd":"Jordan", "addr":"Chicago"},
+    {"id":"Shaquile", "pwd":"O'Neal", "addr":"LA"},
+    {"id":"Kevin", "pwd":"Durant", "addr":"Brooklyn"}
+]
+
+
+def check_func(func):
+    def wrapper(*args, **kwargs):
+        input_id = input("insert ID : ")
+        input_pwd = input("insert Password : ")
+        result = 0;
+        for data in datas:
+            if data["id"] == input_id and data["pwd"] == input_pwd:
+                result = func(*args, **kwargs)
+                return result
+        print("일치하는 정보가 없습니다.")
+    return wrapper
+
+@check_func
+def plus(a, b):
+    return a+b
+
+@check_func
+def get_lotto_number():
+
+    #번호를 담을 리스트를 만든다.
+    number_ls = []
+    while True:
+         number = random.randint(1, 45)
+         if number not in number_ls:
+             number_ls.append(number)
+         if len(number_ls) >= 6:
+             number_ls.sort()
+             break
+    print(number_ls)
+
+plus_result = plus(3,4)
+print(plus_result)
+get_lotto_number()
