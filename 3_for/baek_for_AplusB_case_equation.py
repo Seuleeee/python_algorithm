4b35477f11442b916bd70a3caad062d623410dcd
Seuleeee <jhs931224@gmail.com>
2020-04-08 17:56:45 +0900

    백준 11022번 풀이완료

---
+++ 3_for/baek_for_AplusB_case_equation.py
@@ -0,0 +1,6 @@
+# 백준 11022번 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+import sys
+T = int(sys.stdin.readline())
+for data in range(1, T+1):
+    a, b = map(int, sys.stdin.readline().split())
+    print("Case #{}: {} + {} = {}".format(data, a, b, a+b))
