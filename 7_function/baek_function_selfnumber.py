bbef20f4166380822ae799cde1bf86698c6e4cfe
Seuleeee <jhs931224@gmail.com>
2020-04-20 19:25:44 +0900

    생성자 없는 self number 찾기, 어려웠음

--- 7_function/baek_function_selfnumber.py
+++ 7_function/baek_function_selfnumber.py
@@ -1,17 +1,18 @@
 #백준 4673번
 #1. 생성자를 만드는 수열 만들기
-
+#2. 생성자가 있나없나 판단하기
+#3. 생성자 없는 것 가려내기
 def make_d(a):
     num = str(a)
     sum = a
     for i in range(len(num)):
         sum += int(num[i])
-
     return sum
-print(make_d(1))
 self_num_ls = []
 for i in range(1, 10000):
     result = make_d(i)
-    if i == result:
-        self_num_ls.append(i)
-print(self_num_ls)
+    self_num_ls.append(result)
+self_num = []
+for i in range(1, 10000):
+    if i not in self_num_ls:
+        print(i)
